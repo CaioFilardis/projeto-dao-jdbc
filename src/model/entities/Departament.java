6caf09d46b9ec6bbff3ef995de332e834df6e585
Caio Filardis <caiosantosshake@hotmail.com>
2025-07-15 19:17:33 -0300

    Criando a classe vendedor (seller)

--- src/model/entities/Departament.java
+++ src/model/entities/Departament.java
@@ -5,6 +5,9 @@ import java.util.Objects;
 
 public class Departament implements Serializable {
 
+	
+	private static final long serialVersionUID = 1L;
+	
 	private Integer id;
 	private String name;
 	
