b447568d78c697070cfde90761df3555621e914f
Caio Filardis <caiosantosshake@hotmail.com>
2025-07-17 21:14:40 -0300

    Retornando todos os vendedores ordenados por nome, através do método 'findAll'.

--- src/modal/dao/impl/SellerDaoJDBC.java
+++ src/modal/dao/impl/SellerDaoJDBC.java
@@ -104,7 +104,46 @@ public class SellerDaoJDBC implements SellerDao {
 
 	@Override
 	public List<Seller> findAll() {
-		return null;
+		PreparedStatement st = null;
+		ResultSet rs = null;
+		
+		try {
+			st = conn.prepareStatement(
+					"SELECT seller.*, department.Name AS DepName "
+				    + "FROM seller INNER JOIN department "
+				    + "ON seller.DepartmentId = department.Id "
+				    + "ORDER BY Name"
+					);
+	
+			rs = st.executeQuery(); // resultado
+			
+			List<Seller> list = new ArrayList<>();
+			
+			// apenas executar com base em uma 'chave'(que no caso será o id)
+			Map<Integer, Department> map = new HashMap<>(); 
+			
+			while (rs.next()) {
+				
+				// pegar os departamentos existentes
+				Department dep = map.get(rs.getInt("DepartmentId"));				
+				
+				if (dep == null) {
+					dep = instantiateDepartment(rs);
+					map.put(rs.getInt("DepartmentId"), dep);
+				}
+				
+				Seller obj = instantiateSeller(rs, dep);
+				list.add(obj);
+			}
+			return list;
+		}
+		catch (SQLException e) {
+			throw new DbException(e.getMessage());
+		}
+		finally { // não precisa fechar a conexao
+			DB.closeStatement(st);
+			DB.closeResultSet(rs);
+		}
 	}
 
 
@@ -154,7 +193,4 @@ public class SellerDaoJDBC implements SellerDao {
 			DB.closeResultSet(rs);
 		}
 	}
-
-	
-	
 }
