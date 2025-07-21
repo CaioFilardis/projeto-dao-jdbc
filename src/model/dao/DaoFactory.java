b3d7b92e84c06cc636a78e1b0631ea58b60956e8
Caio Filardis <caiosantosshake@hotmail.com>
2025-07-21 00:09:28 -0300

    Criando as classe 'DepartmentDaoJDBC' para que possa realizar às constultas e/ou implementações na tabela de department. Além de adicionar um método a interface 'DaoFactory' para estabelecer um novo contrato de instancia da conexão ao banco.

--- src/model/dao/DaoFactory.java
+++ src/model/dao/DaoFactory.java
@@ -1,6 +1,7 @@
 package model.dao;
 
 import db.DB;
+import modal.dao.impl.DepartmentDaoJDBC;
 import modal.dao.impl.SellerDaoJDBC;
 
 /* classe auxiliar para instanciar os DAOs*/
@@ -10,5 +11,9 @@ public class DaoFactory {
 	// porém, na sua execução irá instanciar o objeto DAO
 	public static SellerDao createdSellerDao() {
 		return new SellerDaoJDBC(DB.getConnection());
+	}
+	
+	public static DepartmentDao createdDepartmentDao() {
+		return new DepartmentDaoJDBC(DB.getConnection());
 	}
 }
